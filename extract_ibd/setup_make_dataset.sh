1be73d4a7f53781db6915a69064ece048d250c4a
eracah <ejracah@gmail.com>
2016-05-31 14:07:38 -0700

    Changed to setup script to work and added readme to reflect how I got it to work

--- extract_ibd/setup_make_dataset.sh
+++
@@ -1,8 +0,0 @@
-#module swap PrgEnv-intel PrgEnv-gnu
-module unload python
-module unload h5py
-module load python/2.7-anaconda
-module load root 
-export PYTHONPATH=/global/project/projectdirs/das/racah:$PYTHONPATH 
-export PYTHONPATH=/usr/common/software/root/6.02.12/lib/root:/usr/common/software/root/6.02.12/bin:$PYTHONPATH
-
